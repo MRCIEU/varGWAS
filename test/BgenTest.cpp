ace54d80dc2347e585b9ee91eacd5b9b79305076
Matt Lyon <ml18692@bristol.ac.uk>
2021-03-04 12:00:33 +0000

    added openmp

--- test/BgenTest.cpp
+++ test/BgenTest.cpp
@@ -12,7 +12,6 @@ TEST(BgenTest, read_variants_openmp) {
   genfile::bgen::BgenParser bgen_parser("/Users/ml18692/projects/jlst_cpp/lib/bgen/example/example.v11.bgen");
 
   // parse BGEN file
-#pragma omp parallel private(var1, var2) shared(var3)
   while (bgen_parser.read_variant(&chromosome, &position, &rsid, &alleles)) {
     bgen_parser.read_probs(&probs);
     std::cout << chromosome << " " << position << " " << rsid << " " << alleles[0] << " " << alleles[1] << std::endl;
