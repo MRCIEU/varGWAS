b54c94a7792ac25fb7f8fb9f719148591e0578ae
Matt Lyon <ml18692@bristol.ac.uk>
2021-05-05 15:32:08 +0100

    fixed sim

--- sim/funs.R
+++ sim/funs.R
@@ -12,3 +12,12 @@ bp <- function(x, y) {
   f <- tidy(f)
   return(data.frame(BETA_x.r = fit2$estimate[2], SE_x.r = fit2$std.error[2], BETA_xsq.r = fit2$estimate[3], SE_xsq.r = fit2$std.error[3], P.r = f$p.value[2]))
 }
+
+#' Function to simulate genotypes in HWE
+#' @param q Recessive/alternative allele frequency
+#' @param n_obs Number of observations to return
+get_simulated_genotypes <- function(q, n_obs){
+  p <- 1 - q
+  x <- sample(c(0, 1, 2), n_obs, prob=c(p^2, 2 * p * q, q^2), replace=T)
+  return(x)
+}
