158e989835bc1682d63d037e16d0d110584b7b84
Matthew Lyon <matt.lyon@bristol.ac.uk>
2021-09-15 14:17:27 +0100

    Update runR.sh

--- sim/runR.sh
+++ sim/runR.sh
@@ -12,6 +12,6 @@ module load apps/bgen/1.1.6
 
 # run
 mkdir -p data
-PATH="$PATH":/mnt/storage/home/ml18692/projects/jlst_cpp/sim
-PATH="$PATH":/mnt/storage/home/ml18692/projects/jlst_cpp/build/bin
+PATH="$PATH":/mnt/storage/home/ml18692/projects/varGWAS/sim
+PATH="$PATH":/mnt/storage/home/ml18692/projects/varGWAS/build/bin
 Rscript "$@"
