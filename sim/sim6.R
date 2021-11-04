8f0fa0d26d21499970773dc9ce327003aa2f2d2f
Matt Lyon <ml18692@bristol.ac.uk>
2021-11-04 10:59:28 +0000

    added boot

--- sim/sim6.R
+++ sim/sim6.R
@@ -1,37 +1,66 @@
 library("broom")
 library("quantreg")
 library("dplyr")
+library("boot")
 library("car")
 set.seed(23)
 
 n_obs <- 1000
 n_sim <- 200
 
+model <- function(x, y){
+    # LAD regression
+    fit <- suppressWarnings(rq(y ~ x, tau=0.5))
+    # absresi
+    d <- abs(resid(fit))
+    # dummy SNP
+    x <- as.factor(x)
+    # second-stage model
+    fit2 <- lm(d ~ x)
+    # extract coef
+    b <- fit2 %>% tidy %>% dplyr::pull("estimate")
+    return(b)
+}
+
+get_est0 <- function(d, i){
+    b <- model(d$x[i], d$y[i])
+    return(b[1]^2/(2/pi))
+}
+get_est1 <- function(d, i){
+    b <- model(d$x[i], d$y[i])
+    return(b[1]^2/(2/pi) + (2*b[1]*b[2]+b[2]^2)/(2/pi))
+}
+get_est2 <- function(d, i){
+    b <- model(d$x[i], d$y[i])
+    return(b[1]^2/(2/pi) + (2*b[1]*b[3]+b[3]^2)/(2/pi))
+}
+
 results <- data.frame()
-for (b in seq(0, 6, .5)){
+for (b in seq(2)){
     for (i in 1:n_sim){
+        # SNP
         x <- rbinom(n_obs, 2, .6)
+        # modifier
         u <- rnorm(n_obs)
+        # outcome
         y <- x*u*b + rnorm(n_obs)
-
-        fit <- suppressWarnings(rq(y ~ x, tau=0.5))
-        d <- abs(resid(fit))
-        x <- as.factor(x)
-        fit2 <- lm(d ~ x)
-        d1 <- deltaMethod(fit2, "(Intercept)^2/(2/pi) + (2*(Intercept)*x1+x1^2)/(2/pi)", vcov=hccm)
-        d2 <- deltaMethod(fit2, "(Intercept)^2/(2/pi) + (2*(Intercept)*x2+x2^2)/(2/pi)", vcov=hccm)
-        e1 <- d1 %>% dplyr::pull("Estimate")
-        e2 <- d2 %>% dplyr::pull("Estimate")
-        se1 <- d1 %>% dplyr::pull("SE")
-        se2 <- d2 %>% dplyr::pull("SE")
-        lci1 <- d1 %>% dplyr::pull("2.5 %")
-        uci1 <- d1 %>% dplyr::pull("97.5 %")
-        lci2 <- d2 %>% dplyr::pull("2.5 %")
-        uci2 <- d2 %>% dplyr::pull("97.5 %")
-
+        # estimate variance for SNP=1 and boostrap SE
+        bs1 <- boot(data.frame(x, y), get_est1, R=1000, stype="i") %>% tidy
+        # estimate variance for SNP=2 and boostrap SE
+        bs2 <- boot(data.frame(x, y), get_est2, R=1000, stype="i") %>% tidy
+        # extract parameters
+        e1 <- bs1$statistic
+        se1 <- bs1$std.error
+        e2 <- bs2$statistic
+        se2 <- bs2$std.error
+        lci1 <- e1 - (1.96 * se1)
+        uci1 <- e1 + (1.96 * se1)
+        lci2 <- e2 - (1.96 * se2)
+        uci2 <- e2 + (1.96 * se2)
+        # store results
         results <- rbind(results, data.frame(
-            v1=var(y[x==1]),
-            v2=var(y[x==2]),
+            v1=var(y[x==1]), # true variance of SNP=1 group
+            v2=var(y[x==2]), # true variance of SNP=2 group
             e1, e2, se1, se2,
             lci1, uci1,
             lci2, uci2,
@@ -42,7 +71,7 @@ for (b in seq(0, 6, .5)){
 
 # check for coverage of CI
 results %>% dplyr::group_by(b) %>%
-    dplyr::summarize(tidy(binom.test(sum(v1 >= lci1 & v1 <= uci1), n())))
+    dplyr::summarize(tidy(binom.test(sum(v1 >= lci1 & v1 <= uci1), n()))) # count number of times variance of Y is within 95% CI
 
 results %>% dplyr::group_by(b) %>%
-    dplyr::summarize(tidy(binom.test(sum(v2 >= lci2 & v2 <= uci2), n())))
+    dplyr::summarize(tidy(binom.test(sum(v2 >= lci2 & v2 <= uci2), n()))) # count number of times variance of Y is within 95% CI
