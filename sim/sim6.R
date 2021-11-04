8cf088371d83b287c2b50d807dcd79848b291ca0
Matt Lyon <ml18692@bristol.ac.uk>
2021-11-04 16:57:41 +0000

    added bs

--- sim/sim6.R
+++ sim/sim6.R
@@ -25,7 +25,11 @@ get_residual <- function(x, y, covar=NULL){
     return(as.vector(d))
 }
 
-model <- function(x, y){
+model <- function(data, indices){
+    # subset vectors
+    data <- data[indices,]
+    x <- data$x
+    y <- data$y
     # absresi
     d <- abs(get_residual(x, y))
     # dummy SNP
@@ -33,51 +37,42 @@ model <- function(x, y){
     # second-stage model
     fit2 <- lm(d ~ x)
     # extract coef
-    b <- fit2 %>% tidy %>% dplyr::pull("estimate")
-    return(b)
-}
-
-get_est0 <- function(d, i){
-    b <- model(d$x[i], d$y[i])
-    return(b[1]^2/(2/pi))
-}
-get_est1 <- function(d, i){
-    b <- model(d$x[i], d$y[i])
-    return(b[1]^2/(2/pi) + (2*b[1]*b[2]+b[2]^2)/(2/pi))
-}
-get_est2 <- function(d, i){
-    b <- model(d$x[i], d$y[i])
-    return(b[1]^2/(2/pi) + (2*b[1]*b[3]+b[3]^2)/(2/pi))
+    b0 <- fit2 %>% tidy %>% dplyr::filter(term == "(Intercept)") %>% dplyr::pull("estimate")
+    b1 <- fit2 %>% tidy %>% dplyr::filter(term == "x1") %>% dplyr::pull("estimate")
+    b2 <- fit2 %>% tidy %>% dplyr::filter(term == "x2") %>% dplyr::pull("estimate")
+    # difference between 0 vs 1 and 0 vs 2
+    return(c(
+        (2*b0*b1+b1^2)/(2/pi),
+        (2*b0*b2+b2^2)/(2/pi)
+    ))
 }
 
 results <- data.frame()
 for (b in seq(2, 2)){
     for (i in 1:n_sim){
-        message(paste0("b:",b, " i:", i))
+        message(paste0("b:", b, " i:", i))
         # SNP
-        x <- rbinom(n_obs, 2, .6)
+        x <- rbinom(n_obs, 2, .5)
         # modifier
         u <- rnorm(n_obs)
         # outcome
         y <- x*u*b + rnorm(n_obs)
-        # estimate variance for SNP=1 and boostrap SE
-        bs1 <- boot(data.frame(x, y), get_est1, R=500, stype="i") %>% tidy
-        # estimate variance for SNP=2 and boostrap SE
-        bs2 <- boot(data.frame(x, y), get_est2, R=500, stype="i") %>% tidy
+        # estimate variance effects and boostrap SE
+        bs <- boot(data.frame(x, y), model, R=1000, stype="i")
         # extract parameters
-        e1 <- bs1$statistic
-        se1 <- bs1$std.error
-        e2 <- bs2$statistic
-        se2 <- bs2$std.error
+        e1 <- bs %>% tidy %>% dplyr::pull("statistic") %>% dplyr::nth(1)
+        se1 <- bs %>% tidy %>% dplyr::pull("std.error") %>% dplyr::nth(1)
+        e2 <- bs %>% tidy %>% dplyr::pull("statistic") %>% dplyr::nth(2)
+        se2 <- bs %>% tidy %>% dplyr::pull("std.error") %>% dplyr::nth(1)
         lci1 <- e1 - (1.96 * se1)
         uci1 <- e1 + (1.96 * se1)
         lci2 <- e2 - (1.96 * se2)
         uci2 <- e2 + (1.96 * se2)
         # store results
         results <- rbind(results, data.frame(
-            v1=var(y[x==1]), # true variance of SNP=1 group
-            v2=var(y[x==2]), # true variance of SNP=2 group
-            e1, e2, se1, se2,
+            v1=var(y[x==1]) - var(y[x==0]), # true SNP=0 vs SNP=1 variance
+            v2=var(y[x==2]) - var(y[x==0]), # true SNP=0 vs SNP=2 variance
+            e1, e2, se1, se2, # estimated SNP=0 vs SNP=1 & 2 variance
             lci1, uci1,
             lci2, uci2,
             b
@@ -87,7 +82,7 @@ for (b in seq(2, 2)){
 
 # check for coverage of CI
 results %>% dplyr::group_by(b) %>%
-    dplyr::summarize(tidy(binom.test(sum(v1 >= lci1 & v1 <= uci1), n_sim))) # count number of times variance of Y is within 95% CI
+    dplyr::summarize(tidy(binom.test(sum(v1 >= lci1 && v1 <= uci1), n_sim))) # count number of times variance of Y is within 95% CI
 
 results %>% dplyr::group_by(b) %>%
-    dplyr::summarize(tidy(binom.test(sum(v2 >= lci2 & v2 <= uci2), n_sim))) # count number of times variance of Y is within 95% CI
+    dplyr::summarize(tidy(binom.test(sum(v2 >= lci2 && v2 <= uci2), n_sim))) # count number of times variance of Y is within 95% CI
