3d3c0b50029f4a893c3b0f13b41657df07cabaff
Matt Lyon <ml18692@bristol.ac.uk>
2021-11-10 09:24:19 +0000

    updated ocsa

--- sim/sim6b.R
+++ sim/sim6b.R
@@ -7,14 +7,12 @@ n_obs <- 1000
 n_sim <- 200
 
 results <- data.frame()
-for (b in seq(2,2)){
+for (b in seq(0, 6, .5)){
     for (i in 1:n_sim){
-        # SNP
-        x <- rbinom(n_obs, 2, .6)
-        # modifier
+        x <- get_simulated_genotypes(0.4, n_obs)
         u <- rnorm(n_obs)
-        # outcome
         y <- x*u*b + rnorm(n_obs)
+
         # run models
         res <- run_models(data.frame(
             S = paste0("S", seq(1, n_obs)),
@@ -24,33 +22,25 @@ for (b in seq(2,2)){
             stringsAsFactors=F
         ))
         # estimate var(Y|G==1)
-        e1 <- 1 * res$BETA_x.osca_median
-        e2 <- 2 * res$BETA_x.osca_median
+        b1 <- 1 * res$BETA_x.osca_median
+        b2 <- 2 * res$BETA_x.osca_median
         se1 <- 1 * res$SE_x.osca_median
         se2 <- 2 * res$SE_x.osca_median
-        lci1 <- e1 - (1.96 * se1)
-        uci1 <- e1 + (1.96 * se1)
-        lci2 <- e2 - (1.96 * se2)
-        uci2 <- e2 + (1.96 * se2)
+        lci1 <- b1 - (1.96 * se1)
+        uci1 <- b1 + (1.96 * se1)
+        lci2 <- b2 - (1.96 * se2)
+        uci2 <- b2 + (1.96 * se2)
         # estimate var(Y|G==2)
         # store results
         results <- rbind(results, data.frame(
-            v1=var(y[x==1]) - var(y[x==0]), # true SNP=0 vs SNP=1 variance
-            v2=var(y[x==2]) - var(y[x==0]), # true SNP=0 vs SNP=2 variance
-            e1, e2, se1, se2,
+            b,
+            t1=var(y[x==1]) - var(y[x==0]), t2=var(y[x==2]) - var(y[x==0]),
+            b1, b2,
             lci1, uci1,
-            lci2, uci2,
-            b
+            lci2, uci2
         ))
     }
 }
-
-# check for coverage of CI
-results %>% dplyr::group_by(b) %>%
-    dplyr::summarize(tidy(binom.test(sum(v1 >= lci1 & v1 <= uci1), n()))) # count number of times variance of Y is within 95% CI
-
-results %>% dplyr::group_by(b) %>%
-    dplyr::summarize(tidy(binom.test(sum(v2 >= lci2 & v2 <= uci2), n()))) # count number of times variance of Y is within 95% CI
 
 # write out results
 write.table(file="osca-effects.txt", results)
