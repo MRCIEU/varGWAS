f3d11077f56a09ed9079fcfbeeaea301c4052b8e
Matt Lyon <ml18692@bristol.ac.uk>
2021-11-12 15:55:46 +0000

    updated sim

--- sim/sim8.R
+++ sim/sim8.R
@@ -25,6 +25,22 @@ bp_x_dummy_model <- function(data){
     ))
 }
 
+bp_x_model <- function(data){
+    fit1 <- lm(Y ~ X, data=data)
+    data$d <- resid(fit1)^2
+    fit2 <- lm(d ~ X, data=data)
+    fit3 <- lm(d ~ 1, data=data)
+    p <- anova(fit2, fit3) %>% tidy %>% dplyr::pull(p.value) %>% dplyr::nth(2)
+    b0 <- fit2 %>% tidy %>% dplyr::filter(term == "(Intercept)") %>% dplyr::pull(estimate)
+    s0 <- fit2 %>% tidy %>% dplyr::filter(term == "(Intercept)") %>% dplyr::pull(std.error)
+    b1 <- fit2 %>% tidy %>% dplyr::filter(term == "X") %>% dplyr::pull(estimate)
+    s1 <- fit2 %>% tidy %>% dplyr::filter(term == "X") %>% dplyr::pull(std.error)
+    p1 <- fit2 %>% tidy %>% dplyr::filter(term == "X") %>% dplyr::pull(p.value)
+    return(c(
+        b0,s0,b1,s1,p1
+    ))
+}
+
 bp_xsq_model <- function(data){
     data$xsq <- data$X^2
     fit1 <- lm(Y ~ X, data=data)
@@ -62,8 +78,9 @@ for (b in seq(0, 8, 2)){
         
         # fit models
         fit_osca <- run_osca(data, T)
-        fit_x <- bp_x_dummy_model(data)
+        fit_xd <- bp_x_dummy_model(data)
         fit_xsq <- bp_xsq_model(data)
+        fit_x <- bp_x_model(data)
 
         results <- rbind(results, data.frame(
             b,
@@ -74,16 +91,24 @@ for (b in seq(0, 8, 2)){
             b2_xsq=fit_xsq[5],
             s2_xsq=fit_xsq[6],
             p_xsq=fit_xsq[7],
+            b0_xd=fit_xd[1],
+            s0_xd=fit_xd[2],
+            b1_xd=fit_xd[3],
+            s1_xd=fit_xd[4],
+            b2_xd=fit_xd[5],
+            s2_xd=fit_xd[6],
+            p_xd=fit_xd[7],
             b0_x=fit_x[1],
             s0_x=fit_x[2],
             b1_x=fit_x[3],
             s1_x=fit_x[4],
-            b2_x=fit_x[5],
-            s2_x=fit_x[6],
-            p_x=fit_x[7],
+            p_x=fit_x[5],
             v0=var(data$Y[data$X==0]),
             v1=var(data$Y[data$X==1]),
             v2=var(data$Y[data$X==2]),
+            mad0=mad(data$Y[data$X==0]),
+            mad1=mad(data$Y[data$X==1]),
+            mad2=mad(data$Y[data$X==2]),
             b_osca=fit_osca$BETA_x.osca_median,
             s_osca=fit_osca$SE_x.osca_median,
             p_osca=fit_osca$P.osca_median,
@@ -92,3 +117,12 @@ for (b in seq(0, 8, 2)){
 
     }
 }
+
+# get Z score from P value
+results$zest <- qnorm(results$p_x)
+# denominator from Zhu et al 2016
+results$domin <- sqrt(2*results$q*(1-results$q)*(results$n_obs+results$zest*results$zest))
+# beta
+results$best <- results$zest/results$domin
+# std error
+results$seest <- 1/results$domin
