259051730245e581486f51e584a071e8df48d1b6
Matt Lyon <ml18692@bristol.ac.uk>
2021-11-16 16:15:22 +0000

    fx

--- sim/sim12.R
+++ sim/sim12.R
@@ -1,8 +1,19 @@
 library("data.table")
 library("broom")
 library("boot")
+library('optparse')
 source("funs.R")
-set.seed(12345)
+#set.seed(12345)
+
+option_list <- list(
+  make_option(c("-p", "--phi"), type = "numeric", default = NULL, help = "Effect size of interaction relative to main effect"),
+  make_option(c("-i", "--iteration"), type = "numeric", default = NULL, help = "Simulation iteration"),
+  make_option(c("-n", "--n_iter"), type = "numeric", default = NULL, help = "Number of iterations within this sim")
+);
+opt_parser <- OptionParser(option_list = option_list);
+opt <- parse_args(opt_parser);
+
+set.seed(opt$i + round(opt$p + 100))
 
 # Requires OSCA and QCTOOL on PATH
 
@@ -14,10 +25,10 @@ bs <- function(data, indices) {
 }
 
 n_obs <- 1000
-n_sim <- 30
+n_sim <- 200
 af <- 0.4
 
-# main effect size of X on Y detectable with 80% power
+# main effect size of X on Y detectable with 95% power
 delta <- 0.169
 
 # simulate GxE interaction effects and estimate power
@@ -36,14 +47,11 @@ for (phi in seq(0, 6, 3)){
         )
 
         # simulate outcome
-        data$Y <- data$X * data$U * theta + rnorm(n_obs)
-        #data$Y <- rnorm(n_obs, sd=(1 + data$X * theta))
+        data$Y <- data$X * delta + data$U * delta + data$X * data$U * theta + rnorm(n_obs)
         data$Y <- scale(data$Y)
-        #data_s <- data
-        #data_s$Y <- scale(data_s$Y)
 
         # test for variance effect
-        fit_boot <- boot(data=data, statistic=bs, R=30) %>% tidy
+        fit_boot <- boot(data=data, statistic=bs, R=50) %>% tidy
         fit_osca <- run_osca(data, T)
         fit_osca$BETA_x.osca_median <- fit_osca$BETA_x.osca_median / (2/pi)
         fit_osca$SE_x.osca_median <- fit_osca$SE_x.osca_median / (2/pi)
@@ -74,3 +82,42 @@ for (phi in seq(0, 6, 3)){
         results <- rbind(results, res)
     }
 }
+
+write.csv(results, file="sim12.csv")
+
+get_est_ci <- function(results, estimand){
+    results %>%
+        dplyr::group_by(phi) %>%
+        summarise_each_(
+            funs_( 
+                sprintf("t.test(%s) %>% tidy",estimand)
+            ), 
+            vars = vars_to_test)
+
+        dplyr::summarize(t.test(paste0(estimand)) %>% tidy) %>%
+        dplyr::select(phi, estimate, conf.low, conf.high) %>%
+        dplyr::mutate(estimand=!!estimand)
+}
+
+b1_dummy_ci <- get_est_ci(results, "b1_dummy")
+
+
+
+library("ggplot2")
+ggplot(data=results, aes(x=lambda, y=est_power, ymin=est_power_low, ymax=est_power_high, group=phi, color=phi)) +
+    geom_line() + 
+    geom_point() + 
+    geom_errorbar(width=.05) +
+    theme_classic() + 
+    xlab("Sample size inflation factor") + 
+    ylab(paste0("Power (alpha=", 0.05, ")")) +
+    labs(color = expression(phi)) +
+    scale_y_continuous(limits = c(0, 1), breaks = scales::pretty_breaks(n = 5)) +
+    geom_hline(yintercept = 0.05, linetype = "dashed", color = "grey") +
+    geom_hline(yintercept = 0.8, linetype = "dashed", color = "grey") +
+    scale_color_viridis(direction = 1) +
+    facet_grid(dist ~ method) +
+    theme(
+        strip.background = element_blank(),
+        legend.title.align=0.5
+    )
