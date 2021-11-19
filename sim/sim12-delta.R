c8b0adbceacedb181c5cff7acc592bb6c8507940
Matt Lyon <ml18692@bristol.ac.uk>
2021-11-19 15:17:23 +0000

    moveing hc3

--- sim/sim12-delta.R
+++ sim/sim12-delta.R
@@ -15,48 +15,12 @@ option_list <- list(
 opt_parser <- OptionParser(option_list = option_list);
 opt <- parse_args(opt_parser);
 
-
 get_se <- function(grad, vb){
     vG <- t(grad) %*% vb %*% grad
     sqrt(vG)
 }
 
-# LAD-BF variance effects
 dummy_model_delta <- function(x, y, covar=NULL){
-    if (!is.null(covar)){
-        X <- as.matrix(cbind(x, covar))
-    } else {
-        X <- as.matrix(data.frame(x))
-    }
-    # first-stage fit
-    fit <- qrfit(X=X, y=y, tau=.5, method="mm")
-    b <- rbind(fit$b, fit$beta)
-    # predicted
-    X <- cbind(rep(1, nrow(X)), X)
-    fitted <- X %*% b
-    # residual
-    d <- y - fitted
-    # abs residual
-    d <- abs(as.vector(d))
-    # dummy SNP
-    x <- as.factor(x)
-    # second-stage model
-    fit2 <- lm(d ~ x)
-    # delta
-    #b0 <- deltaMethod(fit2, "b0^2/(2/pi)", parameterNames=c("b0", "b1", "b2"), vcov=vcovHC(fit2, type = "HC0"))
-    b1 <- deltaMethod(fit2, "(2*b0*b1+b1^2)/(2/pi)", parameterNames=c("b0", "b1", "b2"), vcov=vcovHC(fit2, type = "HC0"))
-    b2 <- deltaMethod(fit2, "(2*b0*b2+b2^2)/(2/pi)", parameterNames=c("b0", "b1", "b2"), vcov=vcovHC(fit2, type = "HC0"))
-    # variance betas
-    return(data.frame(
-      #b0_dummy=b0$Estimate,
-      #s0_dummy=b0$SE,
-      b1_dummy=b1$Estimate,
-      s1_dummy=b1$SE,
-      b2_dummy=b2$Estimate,
-      s2_dummy=b2$SE
-    ))
-}
-dummy_model_delta_manual <- function(x, y, covar=NULL){
     if (!is.null(covar)){
         X <- as.matrix(cbind(x, covar))
     } else {
@@ -80,43 +44,26 @@ dummy_model_delta_manual <- function(x, y, covar=NULL){
     b0 <- fit2 %>% tidy %>% dplyr::filter(term == "(Intercept)") %>% dplyr::pull("estimate")
     b1 <- fit2 %>% tidy %>% dplyr::filter(term == "x1") %>% dplyr::pull("estimate")
     b2 <- fit2 %>% tidy %>% dplyr::filter(term == "x2") %>% dplyr::pull("estimate")
-    #b0_dm <- deltaMethod(fit2, "b0^2/(2/pi)", parameterNames=c("b0", "b1", "b2"), vcov=vcovHC(fit2, type = "HC0"))
-    b1_dm <- deltaMethod(fit2, "(2*b0*b1+b1^2)/(2/pi)", parameterNames=c("b0", "b1", "b2"), vcov=vcovHC(fit2, type = "HC0"))
-    b2_dm <- deltaMethod(fit2, "(2*b0*b2+b2^2)/(2/pi)", parameterNames=c("b0", "b1", "b2"), vcov=vcovHC(fit2, type = "HC0"))
-    #s0_dummy <- get_se(c(
-    #    2 * b0/(2/pi),
-    #    0,
-    #    0
-    #), vcovHC(fit2, type = "HC0"))
     s1_dummy <- get_se(c(
         0,
         (2 * b0 + 2 * b1)/(2/pi),
         0
-    ), vcovHC(fit2, type = "HC0"))
+    ), vcovHC(fit2, type = "HC3"))
     s2_dummy <- get_se(c(
         0,
         0,
         (2 * b0 + 2 * b2)/(2/pi)
-    ), vcovHC(fit2, type = "HC0"))
-    # variance betas
+    ), vcovHC(fit2, type = "HC3"))
     return(data.frame(
-      #b0_dummy=b0^2/(2/pi),
-      #s0_dummy,
       b1_dummy=(2*b0*b1+b1^2)/(2/pi),
       s1_dummy,
       b2_dummy=(2*b0*b2+b2^2)/(2/pi),
-      s2_dummy,
-      #b0_dm=b0_dm$Estimate,
-      #s0_dm=b0_dm$SE,
-      b1_dm=b1_dm$Estimate,
-      s1_dm=b1_dm$SE,
-      b2_dm=b2_dm$Estimate,
-      s2_dm=b2_dm$SE
+      s2_dummy
     ))
 }
 
 n_obs <- 100000
-n_sim <- 1000
+n_sim <- 2000
 af <- 0.4
 
 # main effect size of X on Y detectable with 95% power
@@ -143,7 +90,7 @@ results <- data.frame()
     #data$Y <- scale(data$Y)
 
     # test for variance effect
-    res <- dummy_model_delta_manual(data$X, data$Y)
+    res <- dummy_model_delta(data$X, data$Y)
 
     # add params
     #res$v0 <- var(data$Y[data$X==0])
