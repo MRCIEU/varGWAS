2fd5bf8f0b93e4a4c2785726e3724f453618e87d
Matt Lyon <ml18692@bc4login1.bc4.acrc.priv>
2021-11-23 11:21:13 +0000

    updated build dir

--- sim/runR.sh
+++ sim/runR.sh
@@ -12,6 +12,6 @@ module load apps/bgen/1.1.6
 
 # run
 mkdir -p data
-PATH="$PATH":/mnt/storage/home/ml18692/projects/varGWAS/build/bin
+PATH="$PATH":/mnt/storage/home/ml18692/projects/varGWAS/bin
 PATH="$PATH":/mnt/storage/home/ml18692/apps/osca
 Rscript "$@"
