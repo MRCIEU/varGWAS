daf326f826c9b0071a8102577fb4cecaf616a35c
Matt Lyon <ml18692@bristol.ac.uk>
2021-11-23 12:49:17 +0000

    updated sim12

--- sim/sim12.R
+++ sim/sim12.R
@@ -6,26 +6,20 @@ source("funs.R")
 set.seed(12345)
 
 option_list <- list(
-  make_option(c("-p", "--phi"), type = "numeric", default = NULL, help = "Effect size of interaction relative to main effect")
+  make_option(c("-p", "--phi"), type = "numeric", default = NULL, help = "Effect size of interaction relative to main effect"),
+  make_option(c("-i", "--int"), type = "numeric", default = NULL, help = "Linear=0, interaction=1")
 );
 opt_parser <- OptionParser(option_list = option_list);
 opt <- parse_args(opt_parser);
 
 # Requires OSCA and QCTOOL on PATH
 
-# function to obtain regression weights
-bs <- function(data, indices) {
-  d <- data[indices,] # allows boot to select sample
-  result <- dummy_model(d$X, d$Y)
-  return(result)
-}
-
-n_obs <- 1000
-n_sim <- 500
+n_obs <- 10000
+n_sim <- 1000
 af <- 0.4
 
-# main effect size of X on Y detectable with 95% power
-delta <- 0.169
+# main effect size of X on Y explaining 5% variance
+delta <- 0.33
 theta <- delta * opt$phi
 
 # simulate GxE interaction effects and estimate power
@@ -42,22 +36,23 @@ for (i in 1:n_sim) {
     )
 
     # simulate outcome
-    data$Y <- data$X * delta + data$U * delta + data$X * data$U * theta + rnorm(n_obs)
+    if (opt$i == 1){
+      data$Y <- data$X * delta + data$U * delta + data$X * data$U * theta + rnorm(n_obs)
+    } else if (opt$i == 0){
+      data$Y <- data$X * delta + data$U * delta + data$X * data$U * 0 + rnorm(n_obs, sd=sqrt(1 + data$X * theta))
+    }
     data$Y <- scale(data$Y)
 
     # test for variance effect
-    fit_boot <- boot(data=data, statistic=bs, R=500) %>% tidy
-    fit_osca <- run_osca(data, T)
-    fit_osca$BETA_x.osca_median <- fit_osca$BETA_x.osca_median / (2/pi)
-    fit_osca$SE_x.osca_median <- fit_osca$SE_x.osca_median / (2/pi)
+    fit <- run_models(data)
+    fit$BETA_x.osca_median <- fit$BETA_x.osca_median / (2/pi)
+    fit$SE_x.osca_median <- fit$SE_x.osca_median / (2/pi)
 
     res <- data.frame(
-        b0_dummy=fit_boot$statistic[1],
-        s0_dummy=fit_boot$std.error[1],
-        b1_dummy=fit_boot$statistic[2],
-        s1_dummy=fit_boot$std.error[2],
-        b2_dummy=fit_boot$statistic[3],
-        s2_dummy=fit_boot$std.error[3],
+        b1_dummy=fit$phi_x1,
+        s1_dummy=fit$se_x1,
+        b2_dummy=fit$phi_x2,
+        s2_dummy=fit$se_x2,
         b1_osca=fit_osca$BETA_x.osca_median * 1,
         s1_osca=fit_osca$SE_x.osca_median * 1,
         b2_osca=fit_osca$BETA_x.osca_median * 2,
@@ -65,7 +60,6 @@ for (i in 1:n_sim) {
     )
 
     # add params
-    #res <- data.frame(p)
     res$v0 <- var(data$Y[data$X==0])
     res$v1 <- var(data$Y[data$X==1])
     res$v2 <- var(data$Y[data$X==2])
@@ -76,4 +70,4 @@ for (i in 1:n_sim) {
     results <- rbind(results, res)
 }
 
-write.csv(results, file=paste0("sim12_", opt$phi, ".csv"))
+write.csv(results, file=paste0("sim12_", opt$phi, "_", opt$i, ".csv"))
