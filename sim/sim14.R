f531c71a4e23f986ec50d5b93fa016ed8a87a010
Matt Lyon <ml18692@bristol.ac.uk>
2021-12-13 14:20:45 +0000

    updated sim12

--- sim/sim14.R
+++ sim/sim14.R
@@ -2,6 +2,7 @@ library("data.table")
 library("broom")
 library("dplyr")
 library("genpwr")
+library("ggplot2")
 source("funs.R")
 set.seed(12345)
 
@@ -9,27 +10,15 @@ n_obs <- 200
 n_sim <- 200
 af <- 0.4
 
-# main effect size of X on Y detectable with 80% power
-delta <- as.numeric(genpwr.calc(calc = "es", model = "linear", ge.interaction = NULL, N = n_obs, k = NULL, MAF = af, Power = 0.8, Alpha = 0.05, sd_y = 1, True.Model = "Additive", Test.Model = "Additive")$ES_at_Alpha_0.05)
-
-# size of interaction relative to main effect
-phi <- 0.5
-
-# interaction effect size
-theta <- delta * phi
-
-# number of modifiers to evaluate
-n_mods <- c(1, 5, 10, 20)
-
 # simulate interaction effects and test using exhausitve pairwise analysis
 results <- data.frame()
-for (lambda in c(5, 50, 500)) {
-  for (n in n_mods) {
+for (lambda in c(1, 10, 100, 1000)) {
+  for (n_mod in c(1, 5, 10, 20)) {
     res <- data.frame()
 
     for (i in 1:n_sim) {
         # simulate modifiers that have no effect on outcome
-        modifiers <- matrix(rnorm((n-1)*n_obs*lambda), n_obs*lambda, n-1)
+        modifiers <- matrix(rnorm((n_mod-1)*n_obs*lambda), n_obs*lambda, n_mod-1)
 
         # simulate covariates
         data <- data.frame(
@@ -40,14 +29,15 @@ for (lambda in c(5, 50, 500)) {
         )
 
         # simulate outcome
-        data$Y <- data$X * delta + data$U * delta + data$X * data$U * theta + rnorm(n_obs * lambda)
+        # interaction term to have 80% power when lambda = 1x
+        data$Y <- data$X * data$U * 0.29 + rnorm(n_obs * lambda)
 
         # interaction test
         lm.fit <- lm(Y ~ X * U, data)
         lm_p <- apply(modifiers, 2, function(U) lm(data$Y ~ data$X * U) %>% tidy %>% dplyr::filter(term == "data$X:U") %>% dplyr::pull(p.value))
 
         # variance test
-        phi_p <- rep(NA, n)
+        phi_p <- rep(NA, n_mod)
         phi_p[1] <- varGWASR::model(data, "X", "Y")$phi_p
 
         res <- rbind(res,
@@ -55,20 +45,22 @@ for (lambda in c(5, 50, 500)) {
                 lm_p=c(lm.fit %>% tidy %>% dplyr::filter(term == "X:U") %>% dplyr::pull(p.value), lm_p),
                 phi_p,
                 lambda,
-                n
+                n_mod
             )
         )
     }
     
     # calculate power
-    results$lm_h1 <- as.numeric(results$lm_p < 0.05)
-    results$phi_h1 <- as.numeric(results$phi_p < 0.05)
-    lm_pr <- tidy(binom.test(sum(results$lm_h1), nrow(results))) %>% dplyr::select(estimate, conf.low, conf.high)
-    phi_pr <- tidy(binom.test(sum(results$phi_h1, na.rm=T), sum(!is.na(results$phi_p)))) %>% dplyr::select(estimate, conf.low, conf.high)
-    lm_pr$n_mod <- n
-    lm_pr$model <- "LM"
-    phi_pr$n_mod <- n
-    phi_pr$model <- "LAD-BF"
+    res$lm_h1 <- as.numeric(res$lm_p < 0.05)
+    res$phi_h1 <- as.numeric(res$phi_p < 0.05)
+    lm_pr <- tidy(binom.test(sum(res$lm_h1), nrow(res))) %>% dplyr::select(estimate, conf.low, conf.high)
+    phi_pr <- tidy(binom.test(sum(res$phi_h1, na.rm=T), sum(!is.na(res$phi_p)))) %>% dplyr::select(estimate, conf.low, conf.high)
+    lm_pr$n_mod <- n_mod
+    lm_pr$lambda <- lambda
+    lm_pr$model <- "Interaction test"
+    phi_pr$n_mod <- n_mod
+    phi_pr$lambda <- lambda
+    phi_pr$model <- "Variance test"
 
     # store results
     results <- rbind(results, lm_pr)
@@ -77,3 +69,18 @@ for (lambda in c(5, 50, 500)) {
 }
 
 # plot results
+results$lambda <- as.factor(results$lambda)
+ggplot(data=results, aes(x=lambda, y=estimate, ymin=conf.low, ymax=conf.high, group=model)) +
+    geom_line() + 
+    geom_point() + 
+    geom_errorbar(width=.05) +
+    theme_classic() + 
+    xlab("Sample size inflation factor") + 
+    ylab(paste0("Power (alpha=", 0.05, ")")) +
+    scale_y_continuous(limits = c(0, 1), breaks = scales::pretty_breaks(n = 5)) +
+    geom_hline(yintercept = 0.8, linetype = "dashed", color = "grey") +
+    facet_grid(n_mod ~ model) +
+    theme(
+        strip.background = element_blank(),
+        legend.title.align=0.5
+    )
