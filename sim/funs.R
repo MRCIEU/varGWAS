e1dcd817a69f5532b912b895eee4ae5e3497321d
Matt Lyon <ml18692@bristol.ac.uk>
2022-03-24 15:14:25 +0000

    added sim16

--- sim/funs.R
+++ sim/funs.R
@@ -226,3 +226,70 @@ dummy_p <- function(x, y, covar=NULL){
     p <- anova(fit_null, fit2) %>% tidy %>% dplyr::pull(p.value) %>% dplyr::nth(2)
     return(p)
 }
+
+#' Test for effect of SNP on outcome variance using the LAD-BF model
+#'
+#' @param data Dataframe of observations
+#' @param x Name of SNP dosage
+#' @param y Name of outcome
+#' @param covar1 Optional vector of covariates to include in the first-stage model
+#' @param covar2 Optional vector of covariates to include in the second-stage model
+#' @return Dataframe containing variance effect for SNP=1 (phi_x1) and SNP=2 (phi_x2) with SE and p and F-stat
+#' @export
+vargwas_model <- function(data, x, y, covar1=NULL, covar2=NULL){
+    if (any(is.na(data))) stop("Dataframe contains NA values")
+    if (!x %in% names(data)) stop(paste0(x, " was not in dataframe"))
+    if (!y %in% names(data)) stop(paste0(y, " was not in dataframe"))
+    if (!all(covar1 %in% names(data))) stop(paste0("Dataframe is missing some of: ", paste0(covar1, collapse=" ")))
+    if (!all(covar2 %in% names(data))) stop(paste0("Dataframe is missing some of: ", paste0(covar2, collapse=" ")))
+    #if (any(data[[x]] > 2) | any(data[[x]] < 0)) stop("X contains values < 0 | > 2")
+    if (!is.numeric(data[[x]])) stop("Genotype must be numeric")
+
+    # prepare first-stage fit matrix
+    if (!is.null(covar1)){
+        X <- data %>% dplyr::select(!!x, !!covar1) %>% as.matrix
+    } else {
+        X <- data %>% dplyr::select(!!x) %>% as.matrix
+    }
+    # first-stage fit
+    fit <- cqrReg::qrfit(X=X, y=data[[y]], tau=.5, method="mm")
+    b <- rbind(fit$b, fit$beta)
+    # predicted
+    X <- cbind(rep(1, nrow(X)), X)
+    fitted <- X %*% b
+    # residual
+    d <- data[[y]] - fitted
+    # abs residual
+    d <- abs(as.vector(d))
+    # second-stage model
+    data[[x]] <- as.factor(round(data[[x]]))
+    if (!is.null(covar2)){
+        X <- data %>% dplyr::select(!!x, !!covar2)
+        fit2 <- lm(d ~ ., data=X)
+        X <- data %>% dplyr::select(!!covar2)
+        fit_null <- lm(d ~ ., data=X)
+        p <- anova(fit_null, fit2) %>% broom::tidy(.) %>% dplyr::pull(p.value) %>% dplyr::nth(2)
+        f <- anova(fit_null, fit2) %>% broom::tidy(.) %>% dplyr::pull(statistic) %>% dplyr::nth(2)
+    } else {
+        X <- data %>% dplyr::select(!!x)
+        fit2 <- lm(d ~ ., data=X)
+        fit_null <- lm(d ~ 1, data=data)
+        p <- anova(fit_null, fit2) %>% broom::tidy(.) %>% dplyr::pull(p.value) %>% dplyr::nth(2)
+        f <- anova(fit_null, fit2) %>% broom::tidy(.) %>% dplyr::pull(statistic) %>% dplyr::nth(2)
+    }
+
+    # deltamethod
+    v1 <- car::deltaMethod(fit2, "(2*b0*b1+b1^2)/(2/pi)", parameterNames=c("b0", "b1", "b2"), vcov=sandwich::vcovHC(fit2, type = "HC0"))
+    v2 <- car::deltaMethod(fit2, "(2*b0*b2+b2^2)/(2/pi)", parameterNames=c("b0", "b1", "b2"), vcov=sandwich::vcovHC(fit2, type = "HC0"))
+
+    res <- data.frame(
+        phi_x1=v1$Estimate,
+        se_x1=v1$SE,
+        phi_x2=v2$Estimate,
+        se_x2=v2$SE,
+        phi_f=f,
+        phi_p=p
+    )
+    
+    return(res)
+}
--- sim/sim16.R
+++ sim/sim16.R
@@ -4,6 +4,7 @@ library("tidyr")
 library("GWASTools")
 library("jlst")
 library("data.table")
+library("varGWASR")
 source("funs.R")
 set.seed(123)
 
@@ -29,11 +30,20 @@ for (i in 1:n_sim){
     U <- rnorm(n_obs)
 
     # outcome
-    Y <- A*.15 + X*.4 + A*U*.7 + U*.14 + rnorm(n_obs)
+    A <- scale(A)
+    X <- scale(X)
+    Y <- A*sqrt(.2) + X*sqrt(.05) + A*U*sqrt(.1) + U*sqrt(.2) + rnorm(n_obs, sd=sqrt(1-(.2+.05+.1+.2)))
+    varY <- var(Y)
+
+    # OLS regress out confounder
+    fit_Y <- lm(Y ~ A, data=data)
+    fit_X <- lm(X ~ A, data=data)
 
     data <- data.frame(
         X,
         Y,
+        Y_A=resid(fit_Y),
+        X_A=resid(fit_X),
         A,
         U,
         AU=A*U,
@@ -42,37 +52,32 @@ for (i in 1:n_sim){
     )
 
     res <- data.frame(
-        rsq.xc=summary(lm(X ~ C))$r.squared,
-        rsq.yc=summary(lm(Y ~ C))$r.squared,
+        varY,
+        rsq.xa=summary(lm(X ~ as.factor(A)))$r.squared,
+        rsq.ya=summary(lm(Y ~ A))$r.squared,
         rsq.yx=summary(lm(Y ~ X))$r.squared,
-        rsq.ycu=summary(lm(Y ~ C*U))$r.squared,
-        beta.yx=lm(Y ~ X) %>% tidy %>% dplyr::filter(term=="X") %>% dplyr::pull("estimate"),
-        beta.yxc=lm(Y ~ X + C) %>% tidy %>% dplyr::filter(term=="X") %>% dplyr::pull("estimate"),
-        beta.yxcsq=lm(Y ~ X + Csq) %>% tidy %>% dplyr::filter(term=="X") %>% dplyr::pull("estimate"),
-        MAF=mean(data$X * .5),
-        p_adj1=get_p(data$X, data$Y, covar1 = data %>% dplyr::select("C") %>% as.data.frame, covar2 = NULL),
-        p_adj2=get_p(data$X, data$Y, covar1 = data %>% dplyr::select("C") %>% as.data.frame, covar2 = data %>% dplyr::select("C") %>% as.data.frame),
-        p_adj3=get_p(data$X, data$Y, covar1 = data %>% dplyr::select("C") %>% as.data.frame, covar2 = data %>% dplyr::select("Csq") %>% as.data.frame),
-        p_adj4=get_p(data$X, data$Y, covar1 = data %>% dplyr::select("C") %>% as.data.frame, covar2 = data %>% dplyr::select("C", "Csq") %>% as.data.frame),
-        p_adj5=get_p(data$X, data$Y, covar1 = data %>% dplyr::select("C", "Csq") %>% as.data.frame, covar2 = NULL),
-        p_adj6=get_p(data$X, data$Y, covar1 = data %>% dplyr::select("C", "Csq") %>% as.data.frame, covar2 = data %>% dplyr::select("C") %>% as.data.frame),
-        p_adj7=get_p(data$X, data$Y, covar1 = data %>% dplyr::select("C", "Csq") %>% as.data.frame, covar2 = data %>% dplyr::select("Csq") %>% as.data.frame),
-        p_adj8=get_p(data$X, data$Y, covar1 = data %>% dplyr::select("C", "Csq") %>% as.data.frame, covar2 = data %>% dplyr::select("C", "Csq") %>% as.data.frame)
-
+        rsq.yau=summary(lm(Y ~ A*U))$r.squared,
+        beta_mu0=lm(Y ~ X) %>% tidy %>% dplyr::filter(term=="X") %>% dplyr::pull("estimate"),
+        beta_mu1=lm(Y ~ X + A) %>% tidy %>% dplyr::filter(term=="X") %>% dplyr::pull("estimate"),
+        p_adj0=vargwas_model(data, "X", "Y", covar1 = NULL, covar2 = NULL) %>% dplyr::pull("phi_p"),
+        p_adj1=vargwas_model(data, "X", "Y", covar1 = c("A"), covar2 = NULL) %>% dplyr::pull("phi_p"),
+        p_adj2=vargwas_model(data, "X", "Y", covar1 = c("A"), covar2 = c("A")) %>% dplyr::pull("phi_p"),
+        p_adj3=vargwas_model(data, "X", "Y", covar1 = c("A"), covar2 = c("Asq")) %>% dplyr::pull("phi_p"),
+        p_adj4=vargwas_model(data, "X", "Y", covar1 = c("A"), covar2 = c("A", "Asq")) %>% dplyr::pull("phi_p"),
+        p_bf0=leveneTest(data$Y, as.factor(data$X), center=median) %>% tidy %>% dplyr::filter(!is.na(p.value)) %>% dplyr::pull(p.value),
+        p_bf1=leveneTest(data$Y_A, as.factor(data$X), center=median) %>% tidy %>% dplyr::filter(!is.na(p.value)) %>% dplyr::pull(p.value)
     )
 
     results <- rbind(results, res)
 }
 
-# test power w/wo adjustment
-write.csv(results, file="sim5.csv")
-
-# adjusted for C in the first-stage model only 
-pdf("data/fs_adj.pdf")
-GWASTools::qqPlot(results$p_adj1,main="First-stage model adjusted for C")
-dev.off()
-
-# adjusted for C in the first-stage model + C & C^2 in the second-stage
-pdf("data/ss_adj.pdf")
-GWASTools::qqPlot(results$p_adj4,main="First-stage model adjusted for C & second-stage adjusted for C + C^2")
-dev.off()
+# estimate T1E
+results %>% dplyr::summarize(
+    binom.test(sum(p_adj0 < 0.05), n_sim) %>% tidy %>% dplyr::select(estimate, conf.low, conf.high) %>% dplyr::rename(p_adj0="estimate", p_adj0.low="conf.low", p_adj0.high="conf.high"),
+    binom.test(sum(p_adj1 < 0.05), n_sim) %>% tidy %>% dplyr::select(estimate, conf.low, conf.high) %>% dplyr::rename(p_adj1="estimate", p_adj1.low="conf.low", p_adj1.high="conf.high"),
+    binom.test(sum(p_adj2 < 0.05), n_sim) %>% tidy %>% dplyr::select(estimate, conf.low, conf.high) %>% dplyr::rename(p_adj2="estimate", p_adj2.low="conf.low", p_adj2.high="conf.high"),
+    binom.test(sum(p_adj3 < 0.05), n_sim) %>% tidy %>% dplyr::select(estimate, conf.low, conf.high) %>% dplyr::rename(p_adj3="estimate", p_adj3.low="conf.low", p_adj3.high="conf.high"),
+    binom.test(sum(p_adj4 < 0.05), n_sim) %>% tidy %>% dplyr::select(estimate, conf.low, conf.high) %>% dplyr::rename(p_adj4="estimate", p_adj4.low="conf.low", p_adj4.high="conf.high"),
+    binom.test(sum(p_bf0 < 0.05), n_sim) %>% tidy %>% dplyr::select(estimate, conf.low, conf.high) %>% dplyr::rename(p_bf0="estimate", p_bf0.low="conf.low", p_bf0.high="conf.high"),
+    binom.test(sum(p_bf1 < 0.05), n_sim) %>% tidy %>% dplyr::select(estimate, conf.low, conf.high) %>% dplyr::rename(p_bf1="estimate", p_bf1.low="conf.low", p_bf1.high="conf.high")
+)
